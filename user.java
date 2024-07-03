d39f80ebe7b02ee31f56f682f8e4a54579c2f982
Adarsh Talla <adarshh226@gmail.com>
2024-07-03 12:16:51 +0530

    changes done

--- user.java
+++ user.java
@@ -349,3 +349,21 @@ public class Kyc {
 
     // getters and setters
 }
+
+package com.socgen.application.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .anyRequest().permitAll(); // Disable all security restrictions
+    }
+}
+
