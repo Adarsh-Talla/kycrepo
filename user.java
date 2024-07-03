b014002c10cf76140fb9466886d1c606e75e06e4
Adarsh Talla <adarshh226@gmail.com>
2024-07-03 12:19:43 +0530

    changes done

--- user.java
+++ user.java
@@ -367,3 +367,26 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 }
 
+
+package com.socgen.application.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .anyRequest().permitAll(); // Disable all security restrictions
+
+        return http.build();
+    }
+}
+
